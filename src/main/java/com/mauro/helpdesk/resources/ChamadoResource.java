0fed42293cd3f3d89ca6defed2e49091a36151c7
maurorossoni <junior.rossonibn@gmail.com>
2023-05-29 22:58:24 -0300

    update para chamado

--- src/main/java/com/mauro/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/mauro/helpdesk/resources/ChamadoResource.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +47,11 @@ public class ChamadoResource {
 		Chamado obj = service.create(objDto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id,@Valid @RequestBody ChamadoDTO objDto) {
+		Chamado newObj = service.update(id, objDto);
+		return ResponseEntity.ok().body(new ChamadoDTO(newObj));
 	}
 }
--- src/main/java/com/mauro/helpdesk/services/ChamadoService.java
+++ src/main/java/com/mauro/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.mauro.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,35 +20,47 @@ import com.mauro.helpdesk.services.exeptions.ObjectNotFoundExcepetion;
 
 @Service
 public class ChamadoService {
-	
+
 	@Autowired
 	private ChamadoRepository repository;
 	@Autowired
 	private TecnicoService tecnicoService;
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundExcepetion("Objeto não encontrado! ID: " + id ));
+		return obj.orElseThrow(() -> new ObjectNotFoundExcepetion("Objeto não encontrado! ID: " + id));
 	}
 
 	public List<Chamado> findAll() {
-				return repository.findAll();
+		return repository.findAll();
 	}
 
-	public Chamado create(@Valid ChamadoDTO objDto) {
-			return repository.save(newChamado(objDto));
+	public Chamado create(@Valid ChamadoDTO obj) {
+		return repository.save(newChamado(obj));
+	}
+
+	public Chamado update(Integer id, @Valid ChamadoDTO objDto) {
+		objDto.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDto);
+		return repository.save(oldObj);
 	}
 
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
 		Cliente cliente = clienteService.findById(obj.getCliente());
-		
+
 		Chamado chamado = new Chamado();
-		if(obj.getId() != null) {
+		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
 		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
+		
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -56,4 +69,5 @@ public class ChamadoService {
 		chamado.setObservacoes(obj.getObservacoes());
 		return chamado;
 	}
+
 }
